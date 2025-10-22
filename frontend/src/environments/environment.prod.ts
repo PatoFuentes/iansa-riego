1d3d3fb9f458839b79ed7fb4beb897fcab53cfc3
Patricio Fuentes <fuentespato2017@gmail.com>
2025-10-22 00:18:06 -0300

    fix: update API URL in production environment configuration

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backend-riego-516546230986.southamerica-west1.run.app',
+  apiUrl: 'https://backend-riego3-516546230986.southamerica-west1.run.app',
 };
