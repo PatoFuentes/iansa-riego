89a3ff3d1d462e3acf47b3ca564d129f71a4cea7
Patricio Fuentes <fuentespato2017@gmail.com>
2025-11-08 20:56:20 -0300

    fix: update database connection port from 3306 to 8080 in server.js

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ socket.on('error', err => {
 const dbHost = process.env.DB_HOST || "localhost";
 const dbConfig = {
   host: dbHost,
-  port: 3306,
+  port: 8080,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
